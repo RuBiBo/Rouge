175f72eb03e16004faa519136f4cdaabd2d110d8
RuBiBo <jack.appelbaum@ymail.com>
2015-01-28 16:46:50 +0100

    Upload first files
    Project start

---
+++ tools/plotcrypt/source/MiniCrypt.js
@@ -0,0 +1,67 @@
+function MiniCrypt () {}
+/*******
+ * stechKey adjust key length
+ */
+MiniCrypt.prototype.strechKey = function fn0(key,length) {
+    var longKey ="";
+    if (length < key.length) {
+        longKey = key.slice(0,length);
+    }
+    if (length == key.length) {
+        longKey = key;
+    }
+    if (length > key.length) {
+        var rounds = length/key.length;
+        var rounds = Math.floor(rounds);
+        var rest = length % key.length;
+        
+        for(var i = 0;i<rounds;i++) {
+            longKey += key;
+        }
+        if (rest > 0) {
+            longKey += key.slice(0,rest);
+        }
+    }
+    //console.log("longKey= "+longKey);
+    return longKey;
+}
+/****
+ * DE = decrypt
+ */
+MiniCrypt.prototype.DE = function fn1 (key, text) {
+    var message = "", currentChar ="", currentKeyChar = "";
+    var strechedKey = this.strechKey(key, text.length);
+    
+    /******
+     * I think a error can happen when x raise over 256
+     */
+    for (i = 0;i < text.length ;i++) {
+        currentKeyChar = strechedKey.charCodeAt(i);
+        currentChar = text.charCodeAt(i);
+        x = parseInt(currentKeyChar) + parseInt(currentChar);
+        //console.log("crypt("+i+")"+currentChar + "+" + currentKeyChar +"="+x);
+        message += String.fromCharCode(x);
+    }
+    
+    return message;
+}
+/*****
+ * EN = encrypt
+ */
+MiniCrypt.prototype.EN = function fn2 (key,text) {
+    var message = ""; 
+    var currentChar ="";
+    var currentKeyChar = "";
+    var strechedKey = this.strechKey(key, text.length);
+    
+    for (i = 0;i < text.length ;i++) {
+        currentKeyChar = strechedKey.charCodeAt(i);
+        currentChar = text.charCodeAt(i);
+        x = parseInt(currentKeyChar) - parseInt(currentChar);
+        x = -1*x;
+        //console.log("enc("+i+")"+currentChar + "-" + currentKeyChar +"="+x);
+        message += String.fromCharCode(x);
+    }
+    
+    return message;
+}
